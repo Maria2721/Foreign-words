b087d989e1ba5fc6437b9c903e032d5fa7adb38f
Maria2721 <84140203+Maria2721@users.noreply.github.com>
2022-09-15 17:05:59 +0300

    add user funct to the table,cond rendering in card

--- foreign-words/src/components/Cardword.jsx
+++ foreign-words/src/components/Cardword.jsx
@@ -6,6 +6,11 @@ import "../styles/Cardword.css";
 
 function Cardword() {
     const [word, setWord] = useState(Array[0]);
+    const [transcr, setTranscr] = useState(false);
+
+    const handleChange = () => {
+        setTranscr(true);
+    }
 
     const arrayRandElement = (arr) => {
         var rand = Math.floor(Math.random() * arr.length);
@@ -24,9 +29,9 @@ function Cardword() {
         <div className='card-train'>
             <div className='card-train-word'>
                 <p>Тема - {word.tags}</p>
-                <p>{word.english}</p>
+                <p className='card-train-word-high'>{word.english}</p>
                 <p>{word.transcription}</p>
-                <p>{word.russian}</p>
+                <p className='card-train-transcr-word'>{transcr ? word.russian : <button onClick={handleChange}>Проверить</button>}</p>
             </div>
             <div className='card-train-button'><button onClick={newWord}>Новое слово</button></div>
         </div>
--- foreign-words/src/components/Oneword.jsx
+++ foreign-words/src/components/Oneword.jsx
@@ -1,20 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import "../styles/Listwords.css";
 
 function Oneword(props) {
-    const [edit, setEdit] = useState(true);
+    const [edit, setEdit] = useState(false);
+    const [cancel, setCancel] = useState(false);
+    const [word, setWord] = useState({
+        mean: `${props.meaning}`,
+        transcr: `${props.transcription}`,
+        transl: `${props.translation}`,
+        sub: `${props.subject}`,
+    })
+    const [oldWord, setOldWord] = useState({
+        mean: '',
+        transcr: '',
+        transl: '',
+        sub: '',
+    })
+
+    const handleEdit = (e) => {
+        setEdit(!edit);
+        setCancel(false);
+        setOldWord({
+            mean: word.mean,
+            transcr: word.transcr,
+            transl: word.transl,
+            sub: word.sub,
+        })
+    }
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setWord({
+            ...word,
+            [name]: value
+        })
+    }
+
+    // more functional in the future => submit newword in bd
+    const handleSave = () => {
+        setEdit(!edit);
+        setOldWord({
+            mean: '',
+            transcr: '',
+            transl: '',
+            sub: '',
+        })
+    }
+
+    const handleСancelEdit = () => {
+        setCancel(true);
+        setEdit(!edit);
+    }
+
+    useEffect(() => {
+        if (cancel == false) {
+            return;
+        }
+        setWord({
+            mean: oldWord.mean,
+            transcr: oldWord.transcr,
+            transl: oldWord.transl,
+            sub: oldWord.sub,
+        })
+    }, [cancel])
 
     return (
-        <tr key={props.id}>
+        <tr key={props.id} value={props.id}>
             <td className='number'>{props.num}</td>
-            <td>{props.meaning}</td>
-            <td>{props.transcription}</td>
-            <td>{props.translation}</td>
-            <td>{props.subject}</td>
-            {edit ? <td className='buttons'><button className="btn-edit"></button><button className="btn-bin"></button></td> :
-                <td className='buttons'><button className="btn-save"></button><button className="btn-cancel"></button></td>}
-        </tr>
+            <td>{edit ? <input type="text" value={word.mean} name="mean" onChange={handleChange} /> : word.mean}</td>
+            <td>{edit ? <input type="text" value={word.transcr} name="transcr" onChange={handleChange} /> : word.transcr}</td>
+            <td>{edit ? <input type="text" value={word.transl} name="transl" onChange={handleChange} /> : word.transl}</td>
+            <td>{edit ? <input type="text" value={word.sub} name="sub" onChange={handleChange} /> : word.sub}</td>
+            {edit ? <td className='buttons'><button className="btn-save" onClick={handleSave}></button><button className="btn-cancel" onClick={handleСancelEdit}></button></td> :
+                <td className='buttons'><button className="btn-edit" onClick={handleEdit}></button><button className="btn-bin"></button></td>
+            }
+        </tr >
     )
 }
 
